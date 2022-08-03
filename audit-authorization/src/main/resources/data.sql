2fd0433f9a580d252d77f4ee59783d463b5dc681
tea-loves-jkdyd <aditi.6ti@gmail.com>
2022-08-03 19:00:33 +0530

    init

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Teja Sri', 'ts', 'ts1234', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Srivalli', 'sv', 'sv1234', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Ishwarya', 'is', 'is1234', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Aditi', 'ad', 'ad1234', 'Project-4');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (5, 'Mohan', 'mn', 'mn1234', 'Project-5');
